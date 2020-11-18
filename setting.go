a828ac6a6fe3757d159c465265b5a232f58453e0
EAHITechnology <45755982+EAHITechnology@users.noreply.github.com>
2020-11-18 16:19:50 +0800

    Update setting.go

--- setting.go
+++ setting.go
@@ -5,8 +5,8 @@ import (
 )
 
 var (
-	DefaultInterval                     int64 = 30
-	DefaultSleepWindow                  int64 = 20
+	DefaultInterval                     int64 = 60
+	DefaultSleepWindow                  int64 = 65
 	DefaultBreakerTestMax                     = 20
 	DefaultErrorPercentThreshold              = 50
 	DefaultBreakerErrorPercentThreshold       = 50
@@ -53,7 +53,7 @@ func (this *breakSettingInfo) SetSleepWindow(sleepWindow int64) *breakSettingInf
 }
 
 /*
-设置周期间隔
+设置采样周期
 */
 func (this *breakSettingInfo) SetInterval(interval int64) *breakSettingInfo {
 	this.Interval = interval
@@ -81,15 +81,18 @@ func (this *breakSettingInfo) AddBreakSetting() (*breaker, error) {
 	if this.ErrorPercentThreshold <= 0 {
 		this.ErrorPercentThreshold = DefaultErrorPercentThreshold
 	}
-	if this.BreakerTestMax <= DefaultBreakerTestMax {
+	if this.BreakerTestMax < DefaultBreakerTestMax {
 		this.BreakerTestMax = DefaultBreakerTestMax
 	}
-	if this.SleepWindow <= DefaultSleepWindow {
-		this.SleepWindow = DefaultSleepWindow
-	}
-	if this.Interval <= DefaultInterval {
+	if this.Interval < DefaultInterval {
 		this.Interval = DefaultInterval
 	}
-	pbreak := newBreaker(this)
-	return pbreak, nil
+	if this.SleepWindow < DefaultInterval {
+		this.SleepWindow = DefaultSleepWindow
+	}
+	if this.Interval%int64(cellCnt) != 0 {
+		return nil, errors.New("interval must 20`s multiple")
+	}
+	pbreaker := newBreaker(this)
+	return pbreaker, nil
 }
